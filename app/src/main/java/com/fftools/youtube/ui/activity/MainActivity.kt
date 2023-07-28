863e4e5d6df1d1914865c8892d9b90a5067065cd
ninh-io <quangninh312456@gmail.com>
2023-07-28 17:34:33 +0700

    28/7

--- app/src/main/java/com/fftools/youtube/ui/activity/MainActivity.kt
+++ app/src/main/java/com/fftools/youtube/ui/activity/MainActivity.kt
@@ -1,16 +1,40 @@
 package com.fftools.youtube.ui.activity
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.ShoppingCart
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.ColorPainter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.fftools.youtube.R
 import com.fftools.youtube.ui.theme.YoutubeTheme
 import com.fftools.youtube.viewmodels.RetrofitModel
 import org.koin.androidx.viewmodel.ext.android.viewModel
@@ -22,14 +46,10 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            YoutubeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+            Surface(
+                modifier = Modifier.fillMaxSize(),
+            ) {
+                HomeScreen()
             }
         }
         initMain()
@@ -61,17 +81,56 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+fun HomeScreen() {
+    Column {
+        BannerCompose()
+        Spacer(modifier = Modifier.height(12.dp))
+        SimpleButton()
+        Spacer(modifier = Modifier.height(12.dp))
+
+    }
+}
+
+@Composable
+fun BannerCompose() {
+    Image(
+        painterResource(id = R.drawable.ic_color),
+        contentDescription = null,
+        modifier = Modifier
+            .shadow(
+                elevation = 1.dp,
+                shape = RoundedCornerShape(size = 30.dp)
+            )
+
     )
 }
 
-@Preview(showBackground = true)
+
 @Composable
-fun GreetingPreview() {
+fun SimpleButton(){
+    Button(
+        onClick = {},
+        colors = ButtonDefaults.buttonColors(
+            containerColor = Color.Black,
+            contentColor = Color.White
+        ),
+        shape = RoundedCornerShape(10.dp)
+    ) {
+
+        Icon(Icons.Default.ShoppingCart, contentDescription = null)
+        Spacer(modifier = Modifier.width(10.dp))
+        Text(
+            text = "Click me",
+        )
+    }
+}
+
+
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun DefaultPreview() {
     YoutubeTheme {
-        Greeting("Android")
+        HomeScreen()
     }
 }
